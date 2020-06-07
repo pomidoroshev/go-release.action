6cc3e7b8eaf43512ef1658a2337e6cd6968ff30b
Luka Zakrajšek <luka@bancek.net>
2020-06-07 16:35:08 +0200

    Fix missing PROJECT_NAME

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,6 @@ if [ -z "${CMD_PATH+x}" ]; then
   export CMD_PATH=""
 fi
 
-FILE_LIST=`/build.sh`
-
-#echo "::warning file=/build.sh,line=1,col=5::${FILE_LIST}"
-
 EVENT_DATA=$(cat $GITHUB_EVENT_PATH)
 echo $EVENT_DATA | jq .
 UPLOAD_URL=$(echo $EVENT_DATA | jq -r .release.upload_url)
@@ -18,6 +14,12 @@ UPLOAD_URL=${UPLOAD_URL/\{?name,label\}/}
 RELEASE_NAME=$(echo $EVENT_DATA | jq -r .release.tag_name)
 PROJECT_NAME=$(basename $GITHUB_REPOSITORY)
 NAME="${NAME:-${PROJECT_NAME}_${RELEASE_NAME}}_${GOOS}_${GOARCH}"
+
+export PROJECT_NAME
+
+FILE_LIST=`/build.sh`
+
+#echo "::warning file=/build.sh,line=1,col=5::${FILE_LIST}"
 
 if [ -z "${EXTRA_FILES+x}" ]; then
 echo "::warning file=entrypoint.sh,line=22,col=1::EXTRA_FILES not set"
